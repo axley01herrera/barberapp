dc587148558053047e08fc6b85e50f7151625505
Axley <axley01herrera@gmail.com>
2023-11-28 12:31:56 +0000

    updates

--- db/db_changes.sql
+++ db/db_changes.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 22-11-2023 a las 16:50:47
--- Versión del servidor: 8.0.31
--- Versión de PHP: 8.2.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `barber_dev`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `config`
---
+#### 
+  ## Table config
+###
 
 DROP TABLE IF EXISTS `config`;
 CREATE TABLE IF NOT EXISTS `config` (
@@ -35,20 +10,18 @@ CREATE TABLE IF NOT EXISTS `config` (
   `theme` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT 'light',
   `currency` varchar(1) COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT '€',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
---
--- Volcado de datos para la tabla `config`
---
+);
 
 INSERT INTO `config` (`id`, `access_key`, `lang`, `theme`, `currency`) VALUES
 (1, '$2y$10$6joTL.Ifjy.UNj4pCgviqe8w0xBtE.yYs7yIxVMtk0BpUuaKSKGTO', 'es', 'light', '€');
 
--- --------------------------------------------------------
+#### 
+  ## End Table config
+###
 
---
--- Estructura de tabla para la tabla `customer`
---
+#### 
+  ## Table customer
+###
 
 DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
@@ -64,13 +37,16 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `emailVerified` int NOT NULL DEFAULT '0',
   `emailSubscription` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+)
 
--- --------------------------------------------------------
+#### 
+  ## End Table customer
+###
 
---
--- Estructura de tabla para la tabla `profile`
---
+
+#### 
+  ## Table profile
+###
 
 DROP TABLE IF EXISTS `profile`;
 CREATE TABLE IF NOT EXISTS `profile` (
@@ -88,20 +64,18 @@ CREATE TABLE IF NOT EXISTS `profile` (
   `zip` int DEFAULT NULL,
   `country` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
---
--- Volcado de datos para la tabla `profile`
---
+);
 
 INSERT INTO `profile` (`id`, `avatar`, `company_name`, `company_type`, `email`, `phone1`, `phone2`, `address1`, `address2`, `city`, `state`, `zip`, `country`) VALUES
 (1, '', 'Demo', 'Barbería', 'axley01herrera@gmail.com', '(+34) 62 72 77 258', '', 'Calle Nepal', '# 16', 'Playa Blanca', 'Las Palmas', 35580, 'España');
 
--- --------------------------------------------------------
+#### 
+  ## End Table profile
+###
 
---
--- Estructura de tabla para la tabla `service`
---
+#### 
+  ## Table service
+###
 
 DROP TABLE IF EXISTS `service`;
 CREATE TABLE IF NOT EXISTS `service` (
@@ -110,19 +84,16 @@ CREATE TABLE IF NOT EXISTS `service` (
   `price` float NOT NULL,
   `description` varchar(999) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
---
--- Volcado de datos para la tabla `service`
---
+)
 
 INSERT INTO `service` (`id`, `title`, `price`, `description`) VALUES
 (1, 'Básico', 10, 'Corte de caballero clásico.'),
 (2, 'Difuminado', 12, 'Corte de caballero con difuminado del cabello.'),
 (3, 'Barba', 5, 'Perfilado de barba al gusto.'),
 (4, 'Cejas', 2, 'Perfilado de cejas.');
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+#### 
+  ## End Table service
+###
+
+
