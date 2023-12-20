f026dc2e99e902ada6267c8ff4476a5cac4afa85
Axley <axley01herrera@gmail.com>
2023-12-20 17:17:29 +0000

    updates

--- app/Views/home/aboutUsModal.php
+++ app/Views/home/aboutUsModal.php
@@ -6,7 +6,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <p class="justify-content-around fs-5"><?php echo $about; ?></p>
+                <p class="fs-5" style="text-align: justify;"><?php echo $about; ?></p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?php echo lang("Text.btn_cancel"); ?></button>
