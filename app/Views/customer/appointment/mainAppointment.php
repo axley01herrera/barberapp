ae205427a471b7eb6a5e533e8895280a1890567e
Axley <axley01herrera@gmail.com>
2023-12-29 16:37:54 +0000

    updates

--- app/Views/customer/appointment/mainAppointment.php
+++ app/Views/customer/appointment/mainAppointment.php
@@ -7,7 +7,7 @@
                 <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">
                     <?php echo lang('Text.cust_appointment_title'); ?>
                 </h1>
-                <div class="fs-6 fw-semibold text-muted"><?php echo lang('Text.cust_dash_page_subtitle'); ?></div>
+                <div class="fs-6 fw-semibold text-muted"></div>
             </div>
             <!-- Page Button Action -->
             <div class="d-flex align-items-center gap-2 gap-lg-3">
