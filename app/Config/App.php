fff16ed526323d51588e486a025e90971784a3ff
Axley <axley01herrera@gmail.com>
2024-01-03 21:38:00 +0000

    upload app prod

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,12 +17,8 @@ class App extends BaseConfig
      *    http://example.com/
      */
 
-    # Production
     public string $baseURL = 'https://negociocitas.online/'; 
-
-    # Dev
-    // public string $baseURL = 'http://barberapp';
-
+    
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
      * If you want to accept multiple Hostnames, set this.
